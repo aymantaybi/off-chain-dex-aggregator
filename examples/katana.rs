331a61953d55ec0ac5b1f20e023d3604feba0f29
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2025-03-16 06:23:28 +0000

    Update Cargo.toml for release optimizations and add ABI JSON files for AggregateRouter, ERC20, and NonfungiblePositionManager

--- examples/katana.rs
+++ examples/katana.rs
@@ -6,25 +6,36 @@ use alloy::{
     primitives::{Bytes, Uint},
     providers::{IpcConnect, ProviderBuilder},
     sol,
-    sol_types::{SolCall, SolValue},
+    sol_types::{SolCall, SolEvent, SolValue},
 };
 use revm::{
-    db::{AlloyDB, CacheDB, EmptyDB},
-    primitives::{address, Account, Address, HashMap, ResultAndState, TxKind, U256},
+    db::{AlloyDB, CacheDB},
+    primitives::{
+        address, Account, Address, ExecutionResult, HashMap, ResultAndState, TxKind, U256,
+    },
     Database, DatabaseRef, Evm,
 };
 
 use off_chain_dex_aggregator::adapters::SwapMode;
 use off_chain_dex_aggregator::{adapters::path::PathAdapter, Aggregator};
-use tokio::sync::mpsc;
 use tracing::info;
-
-use revm_proxy_db::{load_cache_db_from_file, save_cache_db_to_file, NewFetch, ProxyDB};
+use AggregateRouter::execute_1Call;
+use ERC20::Transfer;
 
 sol!(
     #[allow(missing_docs)]
-    #[derive(Debug, PartialEq, Eq)]
-    function execute(bytes calldata commands, bytes[] calldata inputs, uint256 deadline) external payable;
+    #[sol(rpc)]
+    #[derive(Debug)]
+    ERC20,
+    "examples/abi/ERC20.json"
+);
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    #[derive(Debug)]
+    AggregateRouter,
+    "examples/abi/AggregateRouter.json"
 );
 
 static AGGREGATE_ROUTER_ADDRESS: Address = address!("5f0acdd3ec767514ff1bf7e79949640bf94576bd");
@@ -57,32 +68,82 @@ impl PathAdapter for SwapPath {
         DB: Database + DatabaseRef,
         <DB as revm::Database>::Error: std::error::Error + Sync + Send + 'static,
     {
+        let mut token_in = Address::ZERO;
+        let mut token_out = Address::ZERO;
+
         let tx = evm.tx_mut();
+        let caller = tx.caller;
         match self {
-            SwapPath::KatanaV2(pools) => todo!(),
+            SwapPath::KatanaV2(pools) => {
+                token_in = pools[0].token_in;
+                token_out = pools[pools.len() - 1].token_out;
+                let (command, amount_a, amount_b) = match mode {
+                    SwapMode::In => (0x08 as u8, amount, U256::ZERO),
+                    SwapMode::Out => (0x09 as u8, U256::MAX, amount),
+                };
+                let mut path = vec![token_in];
+                for pool in pools {
+                    path.push(pool.token_out);
+                }
+                let input = (caller, amount_a, amount_b, path, true).abi_encode_params();
+                let commands = [command].abi_encode_packed();
+                let call = execute_1Call {
+                    commands: commands.into(),
+                    inputs: vec![input.into()],
+                    deadline: U256::from(32509705735_u64),
+                };
+                tx.transact_to = TxKind::Call(AGGREGATE_ROUTER_ADDRESS);
+                tx.data = call.abi_encode().into();
+            }
             SwapPath::KatanaV3(pools) => {
+                token_in = pools[0].token_in;
+                token_out = pools[pools.len() - 1].token_out;
                 let (command, amount_a, amount_b) = match mode {
                     SwapMode::In => (0x00 as u8, amount, U256::ZERO),
                     SwapMode::Out => (0x01 as u8, U256::MAX, amount),
                 };
                 let path = encode_v3_path(&pools);
-                let input = (tx.caller, amount_a, amount_b, path, true).abi_encode_params();
-
+                let input = (caller, amount_a, amount_b, path, true).abi_encode_params();
                 let commands = [command].abi_encode_packed();
-
-                let call = executeCall {
+                let call = execute_1Call {
                     commands: commands.into(),
                     inputs: vec![input.into()],
                     deadline: U256::from(32509705735_u64),
                 };
-
                 tx.transact_to = TxKind::Call(AGGREGATE_ROUTER_ADDRESS);
                 tx.data = call.abi_encode().into();
-                let ResultAndState { result, state } = evm.transact()?;
-                dbg!(result);
-                todo!()
             }
         };
+
+        let ResultAndState { result, state } = evm.transact()?;
+        let mut amount_in = U256::ZERO;
+        let mut amount_out = U256::ZERO;
+        match result {
+            ExecutionResult::Success {
+                reason,
+                gas_used,
+                gas_refunded,
+                logs,
+                output,
+            } => {
+                for log in logs {
+                    if let Ok(decoded) = Transfer::decode_log(&log, false) {
+                        if token_in == log.address && decoded.from == caller {
+                            amount_in = decoded.value;
+                        } else if token_out == log.address && decoded.to == caller {
+                            amount_out = decoded.value;
+                        };
+                    }
+                }
+            }
+            ExecutionResult::Revert { gas_used, output } => todo!(),
+            ExecutionResult::Halt { reason, gas_used } => todo!(),
+        };
+        let amount = match mode {
+            SwapMode::In => amount_out,
+            SwapMode::Out => amount_in,
+        };
+        Ok((amount, state))
     }
 }
 
@@ -98,60 +159,57 @@ async fn main() -> eyre::Result<()> {
     info!("Provider connected!");
 
     let alloy_db = AlloyDB::new(provider.clone(), BlockId::default()).unwrap();
-    let mut proxy_db = ProxyDB::new(alloy_db);
-    let (sender, mut receiver) = mpsc::unbounded_channel();
-    proxy_db.sender = sender.into();
-    let cache_db = CacheDB::new(proxy_db);
-
-    //let cache_db = load_cache_db_from_file::<EmptyDB>("data/db.json".to_string())?;
-
+    let cache_db = CacheDB::new(alloy_db);
     let evm = Evm::builder()
         .with_db(cache_db)
         .modify_cfg_env(|cfg| {
             cfg.chain_id = 2020;
         })
+        .modify_tx_env(|tx| {
+            tx.caller = address!("c1eb47de5d549d45a871e32d9d082e7ac5d2e3ed");
+        })
         .build();
+
+    let mut paths = vec![];
+
+    let pool_in_path = PathUniswapV3Pool {
+        address: address!("392d372f2a51610e9ac5b741379d5631ca9a1c7f"),
+        token_in: address!("0b7007c13325c48911f73a2dad5fa5dcbf808adc"),
+        token_out: address!("e514d9deb7966c8be0ca922de8a064264ea6bcd4"),
+        fee: Uint::<24, 1>::from(3000),
+    };
+    let path = SwapPath::KatanaV3(vec![pool_in_path]);
+
+    paths.push(path);
 
     let pool_in_path = PathUniswapV2Pool {
         address: address!("4f7687affc10857fccd0938ecda0947de7ad3812"),
         token_in: address!("0b7007c13325c48911f73a2dad5fa5dcbf808adc"),
         token_out: address!("e514d9deb7966c8be0ca922de8a064264ea6bcd4"),
     };
-
     let path = SwapPath::KatanaV2(vec![pool_in_path]);
-    let paths = vec![path];
+
+    paths.push(path);
 
     let mut aggregator = Aggregator { evm, paths };
 
-    let amount = U256::from(1e6);
+    let amount = U256::from(100e6);
 
-    let instant = Instant::now();
-
+    let accounts = aggregator.evm.db().accounts.clone();
+    let now = Instant::now();
     let quote = aggregator.quote(amount, SwapMode::In, 10);
 
-    dbg!(instant.elapsed().as_micros());
+    let sum: U256 = quote.into_iter().sum();
 
-    let mut db = CacheDB::new(EmptyDB::new());
+    dbg!(sum, now.elapsed().as_micros());
 
-    while let Some(fetch) = receiver.recv().await {
-        match fetch {
-            NewFetch::Basic {
-                address,
-                account_info,
-            } => {
-                db.insert_account_info(address, account_info);
-            }
-            NewFetch::Storage {
-                address,
-                index,
-                value,
-            } => {
-                db.insert_account_storage(address, index, value).unwrap();
-            }
-        }
-    }
+    aggregator.evm.db_mut().accounts = accounts;
+    let now = Instant::now();
+    let quote = aggregator.quote(amount, SwapMode::In, 10);
 
-    let _ = save_cache_db_to_file("data/db.json".to_string(), &db);
+    let sum: U256 = quote.into_iter().sum();
+
+    dbg!(sum, now.elapsed().as_micros());
 
     Ok(())
 }
